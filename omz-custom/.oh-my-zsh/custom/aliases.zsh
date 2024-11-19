e0b93aa3a41a567940f5b69b51428cfd84ca24cc
Aurelia Saber <aureliarsaber@gmail.com>
2024-11-19 21:03:38 +0530

    update fzf and fzf-tab config to use transparent tmux popup

--- omz-custom/.oh-my-zsh/custom/aliases.zsh
+++ omz-custom/.oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
 alias cls="clear"
 alias l="eza -l -g --icons -b"
 alias vi="nvim"
+alias fzf='tmux popup -EB -s "none" "fzf"'
--- omz-custom/.oh-my-zsh/custom/fzf-config.zsh
+++ omz-custom/.oh-my-zsh/custom/fzf-config.zsh
@@ -1,4 +1,6 @@
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=gutter:-1,border:#e6e9ef,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
---color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+--color=selected-bg:#494d64 \
+--multi --border=rounded"
