149fbf9913e396ca136c9fa8b3972e583d49527c
Aurelia Saber <aureliarsaber@gmail.com>
2024-11-29 22:12:47 +0530

    move generic config to base

---
+++ omz-custom/.oh-my-zsh/custom/base.zsh
@@ -0,0 +1,13 @@
+# Define path to lesspipe
+export LESSOPEN="| /usr/local/bin/lesspipe.sh %s"
+
+# Define zoxide command
+export ZOXIDE_CMD_OVERRIDE="cd"
+
+# Use bat as a pager for man pages
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+# fix formatting for man pages when using bat as a pager
+export MANROFFOPT="-c"
+
+eval "$(atuin init zsh --disable-up-arrow)"
